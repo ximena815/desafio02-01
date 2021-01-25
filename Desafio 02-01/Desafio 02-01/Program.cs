5fde007e3ad4e4d7367e693f78067fe77f4a1ed7
ximena815 <68305685+ximena815@users.noreply.github.com>
2021-01-24 19:29:47 -0500

    Add files via upload
    crear tres programas cada uno permite resolver (encontrar TODOS los valores) del triangulo rectángulo en condiciones diferentes.
    datos conocidos: y,z.

---
+++ Desafio 02-01/Desafio 02-01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Desafio_02_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entradas
+            Console.Write("Ingrese y: ");
+            double y = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese z: ");
+            double z = double.Parse(Console.ReadLine());
+
+            //proceso
+            //variable t
+            double t = Math.Sqrt(((y)*(y)) + ((z)*(z)));
+            Console.Write("t: " + t);
+
+            //varible c
+            double c = Math.Asin((z / t));
+            double cGrados = c * (180.0 / Math.PI);
+            Console.Write("c: " + cGrados);
+           
+            //variable a
+            double a = 180 - 90 - cGrados;
+            Console.Write("a: " + a);
+
+        }
+    }
+}
